75fe27a9b3659cc9af3b6a6eb48cf146059cea8d
Thanusan-Kanagenthiran <thanushmk6@gmail.com>
2024-10-19 19:33:16 +0530

    Added Security dependencies to gateway server

--- gateway_server/src/main/java/com/tmkproperties/gateway_server/GatewayServerApplication.java
+++ gateway_server/src/main/java/com/tmkproperties/gateway_server/GatewayServerApplication.java
@@ -2,45 +2,12 @@ package com.tmkproperties.gateway_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServerApplication.class, args);
-	}
-
-	@Bean
-	public RouteLocator routeLocator(RouteLocatorBuilder builder) {
-		return builder.routes()
-
-				.route("gateway_server_route", r -> r.path("/gateway_server/**")
-						.filters(f -> f.rewritePath("/gateway_server/(?<remaining>.*)", "/${remaining}")
-								.circuitBreaker(config -> config
-										.setName("gatewayServerCircuitBreaker")
-										.setFallbackUri("forward:/contact-support")
-										.setRouteId("gateway_server_circuitbreaker")))
-						.uri("lb://GATEWAY_SERVER"))
-
-				.route("hotel_route", r -> r.path("/hotel/**")
-						.filters(f -> f.rewritePath("/hotel/(?<remaining>.*)", "/${remaining}")
-								.circuitBreaker(config -> config
-										.setName("hotelCircuitBreaker")
-										.setFallbackUri("forward:/contact-support")
-										.setRouteId("hotel_circuitbreaker")))
-						.uri("lb://HOTEL"))
-
-				.route("booking_route", r -> r.path("/booking/**")
-						.filters(f -> f.rewritePath("/booking/(?<remaining>.*)", "/${remaining}")
-								.circuitBreaker(config -> config
-										.setName("bookingCircuitBreaker")
-										.setFallbackUri("forward:/contactSupport")
-										.setRouteId("booking_circuitbreaker")))
-						.uri("lb://BOOKING"))
-				.build();
 	}
 
 }
