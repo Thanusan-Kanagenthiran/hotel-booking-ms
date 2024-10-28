f232100d11a90312e4c1f128a8c881916201e643
Thanusan-Kanagenthiran <thanushmk6@gmail.com>
2024-10-28 21:04:24 +0530

    API documentation update for Mail server service.

--- mail_server/src/main/java/com/tmkproperties/mail_server/MailServerApplication.java
+++ mail_server/src/main/java/com/tmkproperties/mail_server/MailServerApplication.java
@@ -1,9 +1,31 @@
 package com.tmkproperties.mail_server;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Mail Server API",
+				description = "This is a Mail Server API for " +
+						"TMK Properties - Hotel Management System",
+				version = "v1",
+				contact = @Contact(
+						name = "Thanusan Kanagenthiran",
+						email = "thanushmk6@gmail.com",
+						url = "https://github.com/Thanusan-Kanagenthiran"
+				),
+				license = @License(
+						name = "MIT License",
+						url = "https://github.com/Thanusan-Kanagenthiran/hotel-booking-ms/blob/main/LICENSE"
+				)
+		)
+)
 public class MailServerApplication {
 
 	public static void main(String[] args) {
